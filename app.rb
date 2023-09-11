95ef903fea87cdec824fe226158d3565e57424a6
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-11 21:42:13 +0600

    Add an App class to implement UI modules

---
+++ app.rb
@@ -0,0 +1,27 @@
+require_relative 'modules/book_module'
+require_relative 'modules/game_module'
+require_relative 'modules/music_module'
+require_relative 'modules/genre_module'
+require_relative 'modules/author_module'
+require_relative 'modules/label_module'
+
+
+class App
+  attr_accessor :books, :games, :music, :genre, :author, :label
+
+  def initialize()
+    @books = []
+    @games = []
+    @songs = []
+    @genres = []
+    @authors = []
+    @labels = []
+  end
+
+  include BookModule
+  include GameModule
+  include MusicModule
+  include GenreModule
+  include AuthorModule
+  include LabelModule
+end

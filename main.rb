2afc7dce99dfd0e6b4d50d1464e6730c96fb2158
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-11 22:00:24 +0530

    Add game menu

--- main.rb
+++ main.rb
@@ -1,6 +1,17 @@
 require_relative 'app'
 
 module Main
+  def self.add_game(app)
+    puts "\n"
+    puts 'Is game multiplayes [Yes/No]:'
+    multiplayer = gets.chomp
+    puts 'When is game last played [Enter date format year/month/date]:'
+    last_played_at = gets.chomp
+    puts 'When is game published [Enter date format year/month/date]:'
+    publish_date = gets.chomp
+    app.create_game(multiplayer, last_played_at, publish_date)
+  end
+
   def self.add_book(app)
     puts "\n"
     puts 'Who is the publisher [Enter Publisher Name]:'

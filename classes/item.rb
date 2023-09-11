c19bf934f142619b7386194e1a19e11734217f94
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-11 19:56:51 +0600

    Add genre author and label for relation between the classes

--- classes/item.rb
+++ classes/item.rb
@@ -9,4 +9,19 @@ class Item
         @publish_date = Date.parse(publish_date)
         @archived = archived
     end
+
+    def genre=(genre)
+        @genre = genre
+        genre.items << self unless genre.items.include?(self)
+    end
+
+    def author=(author)
+        @author = author
+        author.items << self unless author.items.include?(self)
+    end
+
+    def label=(label)
+        @label = label
+        label.items << self unless label.items.include?(self)
+    end
 end

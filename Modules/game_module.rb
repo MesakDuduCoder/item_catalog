fba2c501cdc0734ddfa5f5058a92d7a49dc858fa
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-11 20:49:44 +0530

    Add game module

---
+++ Modules/game_module.rb
@@ -0,0 +1,18 @@
+require_relative '../classes/Game/game.rb'
+
+module GameModule
+  def create_game(multiplayer, last_played_at, publish_date)
+    game = Game.new(multiplayer, last_played_at, publish_date)
+    @games << game
+  end
+
+  def list_games
+    if @games.empty?
+      puts 'There are no games'
+    else
+      @games.each_with_index do |game, i|
+        puts "Number: #{i + 1}, Multiplayer: #{game.multiplayer}, last_played_at: #{game.last_played_at}, publish_date: #{game.publish_date}"
+      end
+    end
+  end
+end

7a2d48a63f6ba7e4e221b86d4eb2e5b180d4cc67
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-11 22:43:19 +0530

    Fix files

--- data_manager/game_manager.rb
+++ data_manager/game_manager.rb
@@ -1,14 +1,14 @@
 require 'json'
-require_relative '../classes/Game/game.rb'
+require_relative '../classes/Game/game'
 
 class GameManager
-    DATA_FOLDER = 'JSON/'
+  DATA_FOLDER = 'JSON/'.freeze
 
   def save_games(games)
     File.open("#{DATA_FOLDER}game.json", 'w') do |file|
       data = {
         'Games' => games.map do |game|
-          game_data = {
+          {
             'multiplayer' => game.multiplayer,
             'last_played_at' => game.last_played_at,
             'publish_date' => game.publish_date,
@@ -20,12 +20,14 @@ class GameManager
     end
   end
 
-    def load_games
-        return [] unless File.exist?("#{DATA_FOLDER}game.json")
+  def load_games
+    return [] unless File.exist?("#{DATA_FOLDER}game.json")
 
-        data = JSON.parse(File.read("#{DATA_FOLDER}game.json"))
-        games = []
-        data['Games'].map { |game_data| games << Game.new(game_data['multiplayer'], game_data['last_played_at'], game_data['publish_date']) }
-        games
+    data = JSON.parse(File.read("#{DATA_FOLDER}game.json"))
+    games = []
+    data['Games'].map do |game_data|
+      games << Game.new(game_data['multiplayer'], game_data['last_played_at'], game_data['publish_date'])
     end
+    games
+  end
 end

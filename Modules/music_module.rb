70d9d635f44702c1e9d570977d7f6292f8be52e4
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-11 21:26:49 +0600

    Fix linters

--- Modules/music_module.rb
+++ Modules/music_module.rb
@@ -1,19 +1,19 @@
-require_relative '../classes/Music/music_album.rb'
+require_relative '../classes/Music/music_album'
 
 module MusicModule
-    def create_musicalbum(publish_date, on_spotify)
-        song = MusicAlbum.new(publish_date, on_spotify)
-        @songs << song
-    end
-    
-    def list_musicalbums
-        if @songs.empty?
-          puts 'There are no music album'
-        else
-          @songs.each_with_index do |song, i|
-            puts "Number: #{i + 1}, Is it on spotify?: #{song.on_spotify},
+  def create_musicalbum(publish_date, on_spotify)
+    song = MusicAlbum.new(publish_date, on_spotify)
+    @songs << song
+  end
+
+  def list_musicalbums
+    if @songs.empty?
+      puts 'There are no music album'
+    else
+      @songs.each_with_index do |song, i|
+        puts "Number: #{i + 1}, Is it on spotify?: #{song.on_spotify},
             Publish Date: #{song.publish_date}"
-          end
-        end
+      end
     end
+  end
 end

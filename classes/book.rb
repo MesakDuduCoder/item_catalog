8592198ea62d426b37bec494adb91c69e47ba2c6
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-11 20:20:41 +0600

    Add Book class initialize constructor

---
+++ classes/book.rb
@@ -0,0 +1,12 @@
+require_relative '../item'
+require 'date'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state, :publish_date
+
+    def initialize(publisher, cover_state, publish_date)
+        super(publish_date)
+        @publisher = publisher
+        @cover_state = cover_state
+    end
+end

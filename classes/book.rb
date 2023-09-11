4475e75e48ef25ef4fa2686b0dc3d70923d74f64
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-11 20:24:49 +0600

    Fix linter errors

--- classes/book.rb
+++ classes/book.rb
@@ -4,17 +4,13 @@ require 'date'
 class Book < Item
   attr_accessor :publisher, :cover_state, :publish_date
 
-    def initialize(publisher, cover_state, publish_date)
-        super(publish_date)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
-    
-    def can_be_archived?
-        if super == false || @cover_state == "bad"
-            true
-        else
-            false
-        end
-    end
+  def initialize(publisher, cover_state, publish_date)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super == false || @cover_state == 'bad'
+  end
 end
--- classes/label.rb
+++ classes/label.rb
@@ -1,16 +1,16 @@
 class Label
-  attr_accessor  :title, :color
+  attr_accessor :title, :color
   attr_reader :id, :items
 
   def initialize(title, color)
-      @id = Random.rand(1...1000)
-      @title = title
-      @color = color
-      @items = []
+    @id = Random.rand(1...1000)
+    @title = title
+    @color = color
+    @items = []
   end
 
   def add_item(item)
-      @items << item
-      item.label = self
+    @items << item
+    item.label = self
   end
 end

cf785aebd57c0dd7635a99df48fdfd939bbf6cbb
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-12 12:39:12 +0600

    Fix linter errors

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -6,23 +6,23 @@ describe Book do
     @label = Label.new('title', 'color')
   end
 
-  context "test add item method" do
+  context 'test add item method' do
     it 'should return an array of item' do
-      item = Item.new("2002/01/01", archived: true)
+      item = Item.new('2002/01/01', archived: true)
       @label.add_item(item)
       expect(@label.items).to eq([item])
     end
   end
 
-  context "test user inputs" do
+  context 'test user inputs' do
     it 'should return correct value of title' do
       @label = Label.new('label 1', 'red')
-      expect(@label.title).to eq("label 1")
+      expect(@label.title).to eq('label 1')
     end
 
     it 'should return correct value of color' do
       @label = Label.new('label 1', 'red')
-      expect(@label.color).to eq("red")
+      expect(@label.color).to eq('red')
     end
   end
 end

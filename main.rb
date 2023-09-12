11d02c83881b743825d0229122414b970bd25e50
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-12 15:43:27 +0600

    Updated main.rb by adding main module and converting it into class

--- main.rb
+++ main.rb
@@ -1,45 +1,10 @@
 require_relative 'app'
+require_relative 'modules/main_module'
 
-module Main
-  def self.add_game(app)
-    puts "\n"
-    puts 'Is game multiplayes [Yes/No]:'
-    multiplayer = gets.chomp
-    puts 'When is game last played [Enter date format year/month/date]:'
-    last_played_at = gets.chomp
-    puts 'When is game published [Enter date format year/month/date]:'
-    publish_date = gets.chomp
-    app.create_game(multiplayer, last_played_at, publish_date)
-  end
+class Main
+  include MainModule
 
-  def self.add_mucicalbum(app)
-    puts "\n"
-    puts 'Is it on spotify [True/False]:'
-    on_spotify = gets.chomp
-    if %w[True true].include?(on_spotify)
-      on_spotify = true
-    elsif %w[False false].include?(on_spotify)
-      on_spotify = false
-    else
-      puts 'Invalid input'
-    end
-    puts 'When is album published [Enter date format year/month/date]:'
-    publish_date = gets.chomp
-    app.create_musicalbum(publish_date, on_spotify)
-  end
-
-  def self.add_book(app)
-    puts "\n"
-    puts 'Who is the publisher [Enter Publisher Name]:'
-    publisher = gets.chomp
-    puts 'What Is the cover\'s state like? [Enter good or bad]:'
-    cover_state = gets.chomp
-    puts 'When was the book published [Enter date format year/month/date]:'
-    publish_date = gets.chomp
-    app.create_book(publisher, cover_state, publish_date)
-  end
-
-  def self.list_options
+  def list_options
     puts "\n"
     puts 'Enter number to pick an option:'
     puts '1. List all books'
@@ -54,7 +19,7 @@ module Main
     puts '10. End'
   end
 
-  def self.main
+  def main
     app = App.new
     loop do
       list_options
@@ -77,4 +42,5 @@ module Main
   end
 end
 
-Main.main
+main = Main.new
+main.main

4b932525a1fd7c5525f593ee4e8d2dcb3142951f
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-12 11:27:05 +0600

    Add functionality to read write books into JSON file

--- Modules/book_module.rb
+++ Modules/book_module.rb
@@ -1,19 +1,24 @@
-require_relative '../classes/Book/book'
+require_relative '../classes/Book/book.rb'
 
 module BookModule
   def create_book(publisher, cover_state, publish_date)
     book = Book.new(publisher, cover_state, publish_date)
     @books << book
+    save_book_to_file(@books)
   end
-
+  
   def list_books
     if @books.empty?
       puts 'There are no books'
     else
       @books.each_with_index do |book, i|
-        puts "Number: #{i + 1}) Publisher: #{book.publisher}, Cover state: is in #{book.cover_state} shape,
-        Published in: year #{book.publish_date}"
+        puts "Number: #{i + 1}) Publisher: #{book.publisher}, Cover state: is in #{book.cover_state} shape, Published in: year #{book.publish_date}"
       end
     end
+  end
+
+  
+  def save_book_to_file(books)
+    @book_data.save_books(books)
   end
 end
--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ require_relative 'data_manager/music_manager'
 require_relative 'data_manager/game_manager'
 require_relative 'data_manager/author_manager'
 require_relative 'data_manager/genre_manager'
+require_relative 'data_manager/book_manager'
 
 class App
   attr_accessor :books, :games, :music, :genre, :author, :label
@@ -23,6 +24,7 @@ class App
     @game_data = GameManager.new
     @author_data = AuthorManager.new
     @genre_data = GenreManager.new
+    @book_data = BookManager.new
     load_data
   end
 
@@ -38,5 +40,6 @@ class App
     @games = @game_data.load_games
     @authors = @author_data.load_authors
     @genres = @genre_data.load_genre
+    @books = @book_data.load_books
   end
 end

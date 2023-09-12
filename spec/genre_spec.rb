1416ab1669fbee42facf155a6553249165f79681
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-12 12:56:48 +0600

    Fix linters error

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,23 +1,23 @@
-require_relative '../classes/Music/genre.rb'
-require_relative '../classes/item.rb'
+require_relative '../classes/Music/genre'
+require_relative '../classes/item'
 
 
 describe Genre do
-    before :each do
-      @genre = Genre.new('Rock')
-    end
-  
-    context "test add item method" do
-      it 'should return an array of item' do
-        item = Item.new("2007/03/01", archived: true)
-        @genre.add_item(item)
-        expect(@genre.items).to eq([item])
-      end
-    end
+  before :each do
+    @genre = Genre.new('Rock')
+  end
 
-    context "#test genre" do
-      it 'should return correct name of genre' do
-        expect(@genre.name).to eq("Rock")
-      end
+  context 'test add item method' do
+    it 'should return an array of item' do
+      item = Item.new('2007/03/01', archived: true)
+      @genre.add_item(item)
+      expect(@genre.items).to eq([item])
     end
   end
+
+  context '#test genre' do
+    it 'should return correct name of genre' do
+      expect(@genre.name).to eq('Rock')
+    end
+  end
+end

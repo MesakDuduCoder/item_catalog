4bf8f84d66a4b369c1fff753b203f1c0bc2f8ba4
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-12 12:01:02 +0600

    Fix linter errors

--- data_manager/label_manager.rb
+++ data_manager/label_manager.rb
@@ -2,14 +2,14 @@ require 'json'
 require_relative '../classes/Book/label'
 
 class LabelManager
-    DATA_FOLDER = 'JSON/'
+  DATA_FOLDER = 'JSON/'.freeze
 
-    def load_labels
-        return [] unless File.exist?("#{DATA_FOLDER}label.json")
+  def load_labels
+    return [] unless File.exist?("#{DATA_FOLDER}label.json")
 
-        data = JSON.parse(File.read("#{DATA_FOLDER}label.json"))
-        labels = []
-        data['Lables'].map { |label_data| labels << Label.new(label_data['title'], label_data['color']) }
-        labels
-    end
+    data = JSON.parse(File.read("#{DATA_FOLDER}label.json"))
+    labels = []
+    data['Lables'].map { |label_data| labels << Label.new(label_data['title'], label_data['color']) }
+    labels
+  end
 end
